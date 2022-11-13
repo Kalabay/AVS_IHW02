eb0e75e71ea8d22a0dd592eabe8b9b580c4ad428
Mikhail <mikalabay@edu.hse.ru>
2022-11-13 21:28:11 +0300

    Update checkP.c

--- checkP.c
+++ checkP.c
@@ -5,19 +5,19 @@
 char* GetInput() { // считывние строчки
     int size = 0; // размер строки
     int capacity = 1; // возможный размер
-	char* str = calloc(capacity, sizeof(*str)); // резервируем память
-	char now; // символ сейчас
-	while ((now = getchar()) != EOF) { // считвание до конца строки
-		if (size + 1 > capacity) { // если строка уже не помещается
-			capacity *= 2; // увеличиваем возможный размер
-			str = realloc(str, capacity * sizeof(*str)); // перезаписываем и резервируем память
-		}
-		str[size] = now; // добавили char
-		++size; // увеличили размер строки
+    char* str = calloc(capacity, sizeof(*str)); // резервируем память
+    char now; // символ сейчас
+    while ((now = getchar()) != EOF) { // считвание до конца строки
+	if (size + 1 > capacity) { // если строка уже не помещается
+	    capacity *= 2; // увеличиваем возможный размер
+	    str = realloc(str, capacity * sizeof(*str)); // перезаписываем и резервируем память
+	}
+	str[size] = now; // добавили char
+	++size; // увеличили размер строки
 	}
 	if (size + 1 > capacity) { // если строка уже не помещается
-		capacity *= 2; // увеличиваем возможный размер
-		str = realloc(str, capacity * sizeof(*str)); // перезаписываем и резервируем память
+	    capacity *= 2; // увеличиваем возможный размер
+	    str = realloc(str, capacity * sizeof(*str)); // перезаписываем и резервируем память
 	}
 	str[size] = 0; // символ конца строки
     return str; // вернули строку
